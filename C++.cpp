1727972074619a220b8f4a02d8d800f22a91da75
team9-hcmus <75234933+team9-hcmus@users.noreply.github.com>
2020-12-03 00:02:49 +0700

    Update C++.cpp

--- C++.cpp
+++ C++.cpp
@@ -1,1 +1,27 @@
+using namespace std;
+
+int main()
+{
+    int n,dem=0;
+    cout <<"Nhap so n ";
+    cin>>n;
+
+    for (int i=1;i<=n;i++)
+{
+        if (n%i==0)
+        {
+            dem++;
+        }
+        }
+        if(dem==2)
+            {
+                cout<<"La so nguyen to";
+            }
+        else
+            {
+                cout<<"Ko la so nguyen to";
+            }
+
+    return 0;
+}
 
